c20ed54e01fe9a7788e966c6f3be3538088eb224
Anna <evrika.triz@gmail.com>
2020-12-14 20:08:30 +0300

    Изменен файл login_page.py

--- module_6/pages/login_page.py
+++ module_6/pages/login_page.py
@@ -9,18 +9,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверка URL на корректность
         assert ('http://selenium1py.pythonanywhere.com/' and 'accounts/login/') in self.browser.current_url, \
             "Incorrect URL"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_LINK), "Correct e-mail not presented"
-        assert self.is_element_present(*LoginPageLocators.LOGIN_PSD_LINK), "Wrong password"
-        assert self.is_element_present(*LoginPageLocators.LOGIN_BTN), "Incorrect button"
+        # проверка наличия формы авторизации
+        assert self.check_element_is_present(*LoginPageLocators.place_email_address_locator), "Invalid email address"
+        assert self.check_element_is_present(*LoginPageLocators.place_password_locator), "Incorrect password"
+        assert self.check_element_is_present(*LoginPageLocators.button_login_locator), "Incorrect button"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL_LINK), "Correct e-mail not presented"
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PSD_LINK), "Wrong password"
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PSD_REPEAT_LINK), "Wrong password"
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PSD_REPEAT_LINK), "Incorrect button"
+        # проверка наличия формы регистрации
+        assert self.check_element_is_present(*LoginPageLocators.place_registration_email_locator), "Invalid email address"
+        assert self.check_element_is_present(*LoginPageLocators.place_password_locator), "Incorrect password"
+        assert self.check_element_is_present(*LoginPageLocators.place_password_repead_locator), "Incorrect password"
+        assert self.check_element_is_present(*LoginPageLocators.button_registration_locator), "Incorrect button"
