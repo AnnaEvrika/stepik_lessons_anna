8b7b90b0983d5ea94eeb51fc2ac9c350c8aad481
Anna <evrika.triz@gmail.com>
2020-12-14 20:22:18 +0300

    Изменен файл login_page.py

--- module_6/pages/login_page.py
+++ module_6/pages/login_page.py
@@ -15,12 +15,14 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # проверка наличия формы авторизации
+        assert self.check_element_is_present(*LoginPageLocators.form_login), "Login form is missing on the page"
         assert self.check_element_is_present(*LoginPageLocators.place_email_address_locator), "Invalid email address"
         assert self.check_element_is_present(*LoginPageLocators.place_password_locator), "Incorrect password"
         assert self.check_element_is_present(*LoginPageLocators.button_login_locator), "Incorrect button"
 
     def should_be_register_form(self):
         # проверка наличия формы регистрации
+        assert self.check_element_is_present(*LoginPageLocators.form_register), "Registration form is missing on the page"
         assert self.check_element_is_present(*LoginPageLocators.place_registration_email_locator), "Invalid email address"
         assert self.check_element_is_present(*LoginPageLocators.place_password_locator), "Incorrect password"
         assert self.check_element_is_present(*LoginPageLocators.place_password_repead_locator), "Incorrect password"
