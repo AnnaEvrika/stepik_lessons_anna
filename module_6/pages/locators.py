085c8df872e6646dc285f24dbc075b855973220a
Anna <evrika.triz@gmail.com>
2020-12-14 20:15:36 +0300

    Изменен файл locators.py

--- module_6/pages/locators.py
+++ module_6/pages/locators.py
@@ -2,14 +2,16 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    button_login_or_register_locator = "#login_link"
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators:
+    form_login = (By.CSS_SELECTOR, "#login_form")
     place_email_address_locator = (By.CSS_SELECTOR, "[name='login-username']")
     place_password_locator = (By.CSS_SELECTOR, "[name='login-password']")
     button_login_locator = (By.CSS_SELECTOR, '[name="login_submit"]')
 
+    form_register = (By.CSS_SELECTOR, "#register_form")
     place_registration_email_locator = (By.CSS_SELECTOR, '[name="registration-email"]')
     place_password_registration_locator = (By.CSS_SELECTOR, '[name="registration-password1"]')
     place_password_repead_locator = (By.CSS_SELECTOR, '[name="registration-password2"]')
